ed957f88f22dad0ffedbee2b9ee287f4e408af9a
ammar <akidwai03@gmail.com>
2023-10-17 10:39:51 +0530

    added src

--- 001 Basics/1.3 STL/testingPairs.cpp
+++ 001 Basics/1.3 STL/testingPairs.cpp
@@ -1,15 +1,21 @@
+// link : https://youtu.be/RRVYpIET_RU
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    pair <int, int> p = {1,3};
+    pair <int, int> p = {1,3};                              // stores data in pairs (of any data type)
 
     cout << p.first << " " << p.second;
 
-    pair <int, pair <int, int>> q = {1, {3, 4}};
+    pair <int, pair <int, int>> q = {1, {3, 4}};            // nested pairs
 
     cout << q.first << " " << q.second.second << " " << q.second.first;
+
+    pair <int, int> array[] = {{1,2}, {3, 4}, {5,6}};       // array of pairs
+
+    cout << array[1].second;
 
     return 0;
 }
